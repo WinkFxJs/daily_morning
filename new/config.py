6a86f676791c21e0f26945e65bbd29eea46fc4fe
冯曦 <114207893+WinkFxJs@users.noreply.github.com>
2023-05-08 16:45:08 +0800

    Update config.py

--- new/config.py
+++ new/config.py
@@ -1,22 +1,22 @@
 # 公众号配置
 # 公众号appId
-app_id = "wx176d5c56df579e6b"
+app_id = "wx26ad86a9b3a162a0"
 # 公众号appSecret
-app_secret = "5df4816360a0ab5eb86cda66bfec854e"
+app_secret = "5e034913a62d4aaf232732afc4c1a6cf"
 # 模板消息id
-template_id = "ZoifqEmlmiIFK_Tz3F63Imdws6qHTK-ODnAn_foESmU"
+template_id = "pgH05eVGkdnrLa3S0Gct4ftVlfwsNkrnvVK5Wrg0nG0"
 # 接收公众号消息的微信号
 # 这是我的
-users = "odicq61cUoRuFevQZJiiZ34hla2A,odicq61cUoRuFevQZJiiZ34hla2A"
+users = "ol0Wd5_MDZ0fejX1K0jhix5GWq74"
 # 这是宝宝的
-# user = ""
+user = "ol0Wd5_MDZ0fejX1K0jhix5GWq74"
 
 # 信息配置
 # 所在省份
-province = "湖南"
+province = "安徽"
 # 所在城市
-city = "长沙"
+city = "宣城"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "2022-7-22"
+birthday = "2024-4-11"
 # 在一起的日子，格式同上
-love_date = "2019-1-14"
+love_date = "2023-5-2"
